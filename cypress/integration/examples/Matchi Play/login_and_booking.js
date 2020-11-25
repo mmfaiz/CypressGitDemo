64eae5a4dff6e1cb90eb854a0d96bfdea1bc6672
Faiz <mubasjherfaiz@gmail.com>
2020-11-25 17:29:49 +0100

    testts

--- cypress/integration/examples/Matchi Play/login_and_booking.js
+++ cypress/integration/examples/Matchi Play/login_and_booking.js
@@ -35,9 +35,9 @@ describe('Matchi AB Automation Testing ', function() {
       cy.get('#sport').select('Table tennis',{ force: true }) // select sports type     
       cy.get('#q').click().type("Kronan Sports Club") // search for club
       cy.get('.col-sm-2 > .btn').click() // click smash          
-      //cy.get('.list-inline.no-margin > :nth-child(1) > .btn').click({ multiple: true }) 
-      cy.get('#slots_667 > .list-inline.no-margin > :nth-child(2) > .btn').click({ multiple: true }) // book time    
-      cy.get('#se4ea4f2075bc03f00175bc04cc070237').click({force: true}) // book slot id
+      cy.get('.list-inline.no-margin > :nth-child(1) > .btn').contains('17').click({force: true}) 
+   //   cy.get('#slots_667 > .list-inline.no-margin > :nth-child(2) > .btn').click({ multiple: true }) // book time    
+      cy.get('#se4ea4f2075bc03f00175bc04ccca0414').click({force: true}) // book slot id
       cy.get('.col-sm-8 > :nth-child(3) > .radio > label').click({ force: true}) // payment method
       cy.get('#btnSubmit').click() // submit payment
       cy.get('#userBookingModal > .modal-dialog > .modal-content > .modal-footer > .btn').click() // finish booking
